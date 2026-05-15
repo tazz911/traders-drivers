51e1ae5d54166d3941203df5a12671f1c53c0aa5
SalimAlshibani <godofwar4.sh.sh@gmail.com>
2026-05-15 17:45:13 +0400

    Update RegisterValidation.js

--- client/src/validations/RegisterValidation.js
+++ client/src/validations/RegisterValidation.js
@@ -4,7 +4,7 @@ export const RegisterValidation = yup.object().shape({
     fullName: yup.string().required("Full name is required"),
     email:    yup.string().email("Invalid email format").required("Email is required"),
     phone:    yup.string().required("Phone number is required"),
-    password: yup.string().min(6, "Password must be at least 6 characters").required("Password is required"),
+    password: yup.string().min(8, "Password must be at least 8 characters").required("Password is required"),
     confirmPassword: yup.string()
         .oneOf([yup.ref("password")], "Passwords must match")
         .required("Confirm Password is required"),
